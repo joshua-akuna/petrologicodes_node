8822d2e0109db54997ba5730f5e6c0c332495ea7
Akuna Joshua <akunajoshua@gmail.com>
2024-02-05 05:45:01 +0100

    trying to get the right path to content directory

--- get_posts.js
+++ get_posts.js
@@ -1,15 +1,17 @@
 const config = require("./config")
 const fs = require("fs")
 const {createPost} = require('./create_posts')
+const path = require("path")
 
 const getAllPosts = () =>{
+    console.log(__dirname)
     const posts = fs
-        .readdirSync(config.dev.postsdir)
+        .readdirSync(path.join(__dirname, config.dev.postsdir))
         .map(post => post.slice(0, -3))
         .map(post => createPost(post))
         
-    if (!fs.existsSync(config.dev.outdir)) {
-        fs.mkdirSync(config.dev.outdir)
+    if (!fs.existsSync(path.join(__dirname, config.dev.outdir))) {
+        fs.mkdirSync(path.join(__dirname, config.dev.outdir))
     }
 
     return posts
